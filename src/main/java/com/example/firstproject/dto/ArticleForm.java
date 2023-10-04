96e6db7cf6a05accea301c12c138aca59bc63824
DESKTOP-7P9TNOC\argos <system6805@naver.com>
2023-10-04 13:18:33 +0900

    댓글 기능 생성 전

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 // DTO 클래스이다.
 // DTO는 form 태그에 실어보낸 데이터를 서버의 컨트롤러가 객체에 담아서 받는데 이 객체를 DTO라고 한다.
 // DTO는 즉 DTO는 컨트롤러가 데이터를 사용할때 받는 객체라고 할 수있다.
+
 @AllArgsConstructor // 클래스 안쪽 필드의 모든 생성자를 자동으로 생성해준다
 @ToString // toString() 메서드를 대신해준다.
 // dto 클래스 선언함
--- src/test/java/com/example/firstproject/service/ArticleServiceTest.java
+++ src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -1,10 +1,13 @@
 package com.example.firstproject.service;
 
+import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import lombok.ToString;
+import org.hibernate.grammars.hql.HqlParser;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,6 +20,19 @@ class ArticleServiceTest {
 
     @Autowired
     ArticleService articleService; // 객체 주입 진행함.
+    @Test
+    @Transactional
+    void create_성공_title_content만_있는_dto_입력() {
+        // 1. 예상 데이터
+        String title = "라라라라"; // 임의의 값 배정
+        String content = "4444";  // 임의의 값 배정함.
+        ArticleForm dto = new ArticleForm(null,title,content); // dto 생성함
+        Article expected = new Article(4L,title,content); // 예상 데이터 저장
+        // 2. 실제 데이터
+        Article article = articleService.create(dto); // 실제 데이터를 저장함.
+        // 3. 비교 및 검증
+        assertEquals(expected.toString(), article.toString());
+    }
 
     @Test
     void index() {
@@ -37,6 +53,35 @@ class ArticleServiceTest {
     }
 
     @Test
+    void show_Fail() {
+        // 1. 예상 데이터
+        Long id = -1L;
+        Article expected = null;
+        // 2. 실제 데이터
+        Article article = articleService.show(id);
+        // 3. 비교 및 검증
+        assertEquals(expected,article);
+    }
+
+
+
+    @Test
+    @Transactional // 데이터를 조회(Read)하는 테스트를 제외하고 Create, Update, delete,하는 테스트를 진행할때는 Transacitinoal를 사용하여 롤백시켜 주어야 한다.
+    void create_실패_id가_포함된_dto_입력() {
+        // 1. 예상 데이터 입력
+        Long id = 4L;
+        String title = "라라라라";
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(id,title,content);
+        Article expected = null;
+        // 2. 실제 데이터
+        Article article = articleService.create(dto);
+        // 3. 비교 및 검증
+        assertEquals(expected,article); // null 값은 toString()을 사용할 수없다.
+    }
+
+
+    @Test
     void show_Success () {
         // 1. 예상 데이터
         Long id = 1L;
@@ -47,15 +92,81 @@ class ArticleServiceTest {
         assertEquals(expected.toString(),article.toString());
     }
 
+
     @Test
-    void show_Fail() {
+    @Transactional
+    void update_성공_존재하는_id와_title_content가_있는_dto_입력() {
         // 1. 예상 데이터
-        Long id = -1L;
-        Article expected = null;
+        Long id = 3L;
+        String title = "가나다라";
+        String content = "3343";
+        ArticleForm dto = new ArticleForm(id,title,content); // 실제 데이터를 위한 dto 설정
+        Article expected = new Article(id,title,content); // 예상 값
+
         // 2. 실제 데이터
-        Article article = articleService.show(id);
+        Article article = articleService.update(id,dto);
+
+        // 3. 비교 및 검증
+        assertEquals(expected.toString(),article.toString());
+    }
+
+    @Test
+    @Transactional
+    void update_성공_존재하는_id와_title만_있는_dto_입력() {
+        // update를 하려면 대상의 id를 알아야하고, 변경할 내용을 넣어줘야한다.
+        // 1. 예상 데이터
+        Long id = 3L; // 대상의 아이디
+        String title = "가나다"; // 변경사항
+        String content = "null"; // 변경사항
+        ArticleForm dto = new ArticleForm(id,title,content); // 실제 데이터를 위한 dto 설정
+        Article expected = new Article(id,title,content); // 예상 값
+
+        // 2. 실제 데이터
+        Article article = articleService.update(id,dto);
+
+        // 3. 비교 및 검증
+        assertEquals(expected.toString(),article.toString());
+    }
+
+    @Test
+    @Transactional
+    void update_실패_존재하지않는_id_dto_입력() {
+        // 1. 예상 데이터
+        Long id = 4L;
+        String title = "가나다";
+        String content = "null";
+        ArticleForm dto = new ArticleForm(id,title,content); // 실제 데이터를 위한 dto 설정
+        Article expected = null; // 예상 값
+
+        // 2. 실제 데이터
+        Article article = articleService.update(id,dto);
+
         // 3. 비교 및 검증
         assertEquals(expected,article);
     }
-    // committ 용 변경사항
+
+
+    @Test
+    @Transactional
+    void delete_성공_존재하는_id_입력() {
+        // 1. 예상 데이터 입력
+        Long id = 2L;
+        Article expected = new Article(id,"나나나나","2222");
+        // 2. 실제 데이터
+        Article article = articleService.delete(id);
+        // 3. 비교 및 검증
+        assertEquals(expected.toString(),article.toString());
+    }
+
+    @Test
+    @Transactional
+    void delete_실패_존재하지않는_id_입력() {
+        // 1. 예상 데이터 입력
+        Long id = 4L;
+        Article expected = null;
+        // 2. 실제 데이터
+        Article article = articleService.delete(id);
+        // 3. 비교 및 검증
+        assertEquals(expected,article);
+    }
 }
